ad68ebacc316a4d502f4d643185bbd7f0c775fc2
Vishay Singh <vishay1998@gmail.com>
2023-07-12 11:47:08 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,22 @@ class BaseModel():
     This class serves as a base model for all other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         This method instantiates the object and initializes the public instance attributes
         id, created_at, updated_at
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    exec("self." + key + '=value')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ Unittests for basemodel class
 """
 
 import unittest
+import datetime
 from models import base_model
 
 class TestBaseModel(unittest.TestCase):
@@ -11,18 +12,12 @@ class TestBaseModel(unittest.TestCase):
     This class defines unit tests for the BaseModel class
     """
 
-    def setUp(self):
-        """
-        This method is called before every test is run
-        """
-
-        self.bm = base_model.BaseModel()
-
     def test_create_instance(self):
         """
         Test the creation of an instance
         """
 
+        self.bm = base_model.BaseModel()
         self.assertTrue(self.bm.__class__.__name__, "BaseModel")
 
     def test_save(self):
@@ -30,6 +25,7 @@ class TestBaseModel(unittest.TestCase):
         This method tests the save functionality
         """
 
+        self.bm = base_model.BaseModel()
         self.bm.save()
         self.assertGreater(self.bm.updated_at, self.bm.created_at)
 
@@ -38,7 +34,24 @@ class TestBaseModel(unittest.TestCase):
         This method tests the uuid type
         """
 
+        self.bm = base_model.BaseModel()
         self.assertEqual((type(self.bm.id)), str)
+
+    def test_create_from_dict(self):
+        """
+        This method tests instance creation from dict
+        """
+
+        my_model_json = {'id': '56d43177-cc5f-4d6c-a0c1-e167f8c27337', 
+                'created_at': '2017-09-28T21:03:54.052298', 
+                '__class__': 'BaseModel', 
+                'my_number': 89, 
+                'updated_at': '2017-09-28T21:03:54.052302', 
+                'name': 'My_First_Model'}
+        self.bm = base_model.BaseModel(**my_model_json)
+        self.bm2 = base_model.BaseModel()
+        self.assertEqual(type(self.bm.created_at), datetime.datetime)
+        self.assertNotEqual(self.bm, self.bm2)
 
     def tearDown(self):
         """
